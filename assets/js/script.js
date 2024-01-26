8d7cf326769f73240bca5a880d37d00526c18b16
Priyanka23-Brs <144432559+Priyanka23-Brs@users.noreply.github.com>
2024-01-26 17:14:15 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,13 @@ function setTheme(theme) {
 }
 setTheme(localStorage.getItem("movie-theme") || chathams_blue);
 
-       
+function validatePhoneNumber() {
+  var phoneInput = document.getElementById("phoneInput");
+  var phoneNumber = phoneInput.value.trim();
+  // Check if the phone number is empty or doesn't match the pattern
+  if (phoneNumber === '' || !/^[0-9]{3}[0-9]{3}[0-9]{4}$/.test(phoneNumber)) {
+    alert("Please enter a valid phone number.");
+  } else {
+    alert("Phone number is valid: " + phoneNumber);
+  }
+}   
