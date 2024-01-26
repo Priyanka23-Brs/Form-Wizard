224e9d8a89130a17462fd39084fcaecf59c75223
Priyanka23-Brs <144432559+Priyanka23-Brs@users.noreply.github.com>
2024-01-26 15:50:42 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,4 @@ function setTheme(theme) {
 }
 setTheme(localStorage.getItem("movie-theme") || chathams_blue);
 
-<script>
-        function validatePhoneNumber() {
-            // Get the phone number input value
-            var phoneNumber = document.getElementById('phone').value;
-
-            // Define the regular expression for the desired format
-            var regex = /^\+91\s\d{10}$/;
-
-            // Test the phone number against the regular expression
-            if (regex.test(phoneNumber)) {
-                alert('Phone number is valid!');
-            } else {
-                alert('Invalid phone number! Please use the format: +91 9999999999');
-            }
-        }
-    </script>
+       
